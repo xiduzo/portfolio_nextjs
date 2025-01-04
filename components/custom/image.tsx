d3fadd2ce388fbc664ebb3097bee8195ef68a640
Sander <mail@sanderboer.nl>
2025-01-04 01:06:16 +0100

    fix: dont force blur hash
    it will break the build

--- components/custom/image.tsx
+++ components/custom/image.tsx
@@ -4,8 +4,6 @@ import NextImage, { ImageProps } from "next/image";
 export async function Image(props: Props) {
     return (
         <NextImage
-            placeholder="blur"
-            blurDataURL={await dynamicBlurDataUrl(props.src as string)}
             {...props}
             className={image({
                 className: props.className,
@@ -19,34 +17,3 @@ type Props = ImageProps;
 const image = cva(
     "intersect-once intersect:motion-preset-blur-up-md intersect:motion-delay-500 max-w-[100%]",
 );
-
-const baseUrl =
-    process.env.NODE_ENV === "development"
-        ? "http://localhost:3000/"
-        : process.env.NEXT_PUBLIC_DOMAIN;
-
-export async function dynamicBlurDataUrl(url: string) {
-    const base64str = await fetch(
-        `${baseUrl}/_next/image?url=${url}&w=16&q=75`,
-    ).then(async (res) =>
-        Buffer.from(await res.arrayBuffer()).toString("base64"),
-    );
-
-    const blurSvg = `
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 5'>
-      <filter id='b' color-interpolation-filters='sRGB'>
-        <feGaussianBlur stdDeviation='1' />
-      </filter>
-
-      <image preserveAspectRatio='none' filter='url(#b)' x='0' y='0' height='100%' width='100%'
-      href='data:image/avif;base64,${base64str}' />
-    </svg>
-  `;
-
-    const toBase64 = (str: string) =>
-        typeof window === "undefined"
-            ? Buffer.from(str).toString("base64")
-            : window.btoa(str);
-
-    return `data:image/svg+xml;base64,${toBase64(blurSvg)}`;
-}
